fa71ec88c984802c1e910302fe48f812373dcbc0
Seple <armani1984snajpi@gmail.com>
2025-04-19 23:50:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ NO_OPTIMIZATION_LIST_FILE = "No_Optimization_List.txt"
 OUTPUT_FILE = "Full_DNS_Block.txt"
 OPTIMIZATION_LOG_FILE = "Optimization_suggestion.txt"
 
-THRESHOLD = 50
+THRESHOLD = 100
 
 urls = [
     # 1Hosts Pro
