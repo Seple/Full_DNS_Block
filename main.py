559e1276d92a01e1f0b62e32e610a9b66ade6904
Seple <armani1984snajpi@gmail.com>
2025-10-04 04:56:19 +0200

    Update main.py

--- main.py
+++ main.py
@@ -21,6 +21,10 @@ urls = [
     # "https://adguardteam.github.io/HostlistsRegistry/assets/filter_14.txt",
     # Steven Black List (FP)
     # "https://adguardteam.github.io/HostlistsRegistry/assets/filter_33.txt",
+    # Peter Lowe Blocklist
+    # "https://adguardteam.github.io/HostlistsRegistry/assets/filter_3.txt",
+    # Dan Pollock List
+    "https://adguardteam.github.io/HostlistsRegistry/assets/filter_4.txt",
     # Malicious URL Blocklist
     "https://adguardteam.github.io/HostlistsRegistry/assets/filter_11.txt",
     # Phishing URL Blocklist
